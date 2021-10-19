f66b153f4622af59b0b2dee45a5823a4cec3111e
tuchtenh <50889613+tuchtenh@users.noreply.github.com>
2021-10-19 20:37:23 +0200

    Update mZEDDetection.cpp
    Code cleanup

--- mZEDDetection.cpp
+++ mZEDDetection.cpp
@@ -109,65 +109,16 @@ void mZEDDetection::Update()
       // copy input image to output image
       rrlib::rtti::GenericOperations<rrlib::coviroa::tImage>::DeepCopy((*in_img)[0], *img_out);
       // use the tImage data acs an cv::Mat (does not copy the data)
-
-
-
-
+      
       //___________________________________________________________________
       // Detection of red side lines
-
       cv::Mat out = rrlib::coviroa::AccessImageAsMat(*img_out);
       cv::Mat outred = rrlib::coviroa::AccessImageAsMat(*img_out);
       // apply gaussian blur
-      // img.convertTo(out,-1,1,50);
-      /*if ((old_left > 500 && old_right < -500) || (old_left < -500 || old_right > 500))
-            {
-              if (contrast_change)
-              {
-                contrast -= 0.25;
-                if (contrast > 2)
-                {
-                  contrast_change = false;
-                }
-              }
-              if (!contrast_change)
-              {
-                contrast += 0.25;
-                if (contrast < 0.5)
-                {
-                  contrast_change = true;
-                }
-              }
-            }*/
-
       cv::cvtColor(outred, outred, cv::COLOR_BGR2HSV);
       cv::Mat hsvchannel[3], saturated;
       cv::split(outred, hsvchannel);
       cv::Mat binaryred;
-
-      /*
-      hsvchannel[1] = hsvchannel[1] * 4.5;      // 2
-      std::vector<cv::Mat> hsv = {hsvchannel[0], hsvchannel[1], hsvchannel[2]};
-      cv::merge(hsv, saturated);
-      cv::cvtColor(saturated, outred, cv::COLOR_HSV2BGR);
-      // TODO cv::imwrite("bildSATURATED.png", outred);
-      outred.convertTo(outred, -1, contrast, 0);       // 2
-      // TODO cv::imwrite("bildContrast.png", outred);
-      //cv::Mat binaryred;
-
-      cv::GaussianBlur(outred, outred, cv::Size(5, 5), 0);
-
-      //inverted
-      cv::bitwise_not(outred, outred);
-      //cv::imwrite("inverted.png", outred);
-
-      cv::cvtColor(outred, outred, cv::COLOR_BGR2HSV);
-      // range red
-      // cv::inRange(outred, cv::Scalar(1, 200, 200), cv::Scalar(15, 255, 255), binaryred);
-      // range with inverted
-      cv::inRange(outred, cv::Scalar(85, 120, 230), cv::Scalar(95, 255, 255), binaryred);
-      // TODO cv::imwrite("bildMaskeRot.png", binaryred);
-        */
 
       if (colorSwitchEnable.Get() == true)
       {
@@ -205,8 +156,6 @@ void mZEDDetection::Update()
         // TODO cv::imwrite("bildSATURATED.png", outred);
         outred.convertTo(outred, -1, contrast, 0);       // 2
         // TODO cv::imwrite("bildContrast.png", outred);
-        //cv::Mat binaryred;
-
         cv::GaussianBlur(outred, outred, cv::Size(5, 5), 0);
 
         //inverted
@@ -231,7 +180,6 @@ void mZEDDetection::Update()
         // TODO cv::imwrite("bildSATURATED.png", outred);
         outred.convertTo(outred, -1, contrast, 0);       // 2
         // TODO cv::imwrite("bildContrast.png", outred);
-        //cv::Mat binaryred;
 
         cv::GaussianBlur(outred, outred, cv::Size(5, 5), 0);
 
@@ -245,10 +193,7 @@ void mZEDDetection::Update()
         gui_colorSwitch.Publish(true);
       }
 
-
-
       cv::cvtColor(outred, outred, cv::COLOR_HSV2BGR);
-
       cv::cvtColor(outred, outred, cv::COLOR_BGR2GRAY);
       cv::threshold(outred, outred, 128, 255, cv::THRESH_BINARY);
 
@@ -256,23 +201,10 @@ void mZEDDetection::Update()
       cv::Mat maskred = cv::Mat::zeros(outred.size(), outred.type());
       cv::Point ptsred[4] =
       {
-        /*
-          cv::Point(0, 270), // links oben
-          cv::Point(672, 270), // rechts oben
-          //cv::Point(490, 340), // rechts oben
-          //cv::Point(520, 270), // rechts oben RC_UNIMOG
-          //cv::Point(520, 376), //rechts unten RC_UNIMOG
-          cv::Point(672, 376), //rechts unten
-          //cv::Point(605, 376), //rechts unten
-          cv::Point(0, 376) //links unten
-        */
-
         cv::Point(left_red_x, left_red_y), // 270 links oben
         cv::Point(right_red_x, right_red_y), // 270 rechts oben
         cv::Point(right_red_x, 376), //rechts unten
         cv::Point(left_red_x, 376) //links unten
-
-
       };
 
       // Create a binary polygon mask
@@ -299,49 +231,14 @@ void mZEDDetection::Update()
       //masking the image white
       cv::Mat mask = cv::Mat::zeros(out.size(), out.type());
 
-      /*
-            a = m * (M + 70);
-
-            if( a<=0 || a >400)
-            {
-              a = 0;
-            }
-
-
-            std::cout << "a: " << a << std::endl;
-      */
       a = 0;
 
       cv::Point pts[4] =
       {
-
         cv::Point(left_white_x + a, left_white_y), // links oben
         cv::Point(right_white_x + a, right_white_y), // rechts oben
         cv::Point(right_white_x + a, 376), //rechts unten
         cv::Point(left_white_x + a, 376) //links unten
-        /*
-          cv::Point(135, 330), // links oben
-          cv::Point(340, 330), // rechts oben
-          //cv::Point(490, 340), // rechts oben
-          //cv::Point(520, 270), // rechts oben RC_UNIMOG
-          //cv::Point(520, 376), //rechts unten RC_UNIMOG
-          cv::Point(340, 376), //rechts unten
-          //cv::Point(605, 376), //rechts unten
-          cv::Point(135, 376) //links unten
-          */
-
-        /*
-         cv::Point(left_white_x, left_white_y), // links oben
-         cv::Point(right_white_x, right_white_y), // rechts oben
-         cv::Point(right_white_x, 376), //rechts unten
-         cv::Point(left_white_x, 376) //links unten
-        */
-
-
-
-
-
-
       };
 
       // Create a binary polygon mask
@@ -355,16 +252,11 @@ void mZEDDetection::Update()
       std::vector<cv::Vec4i> lines; // will hold the results of the detection
       cv::HoughLinesP(out, lines, 1, CV_PI / 180, 25, 0, 0); // runs the actual detection
 
-
       //___________________________________________________________________
-
-      // line categorization
+      // Line categorization
       std::vector<cv::Vec4i> right_lines, left_lines, mid_lines;
       std::vector<cv::Point> left_pts, right_pts, mid_pts;
       cv::Vec4d right_line, left_line, mid_line;
-      //double left, right;
-      //left = 250; //230    // 280
-      //right = 340;    // 430
       cv::Point right_b;
       double right_m;
       cv::Point left_b;
@@ -408,8 +300,6 @@ void mZEDDetection::Update()
 
       // TODO cv::imwrite("bildHOUGHLINES.png", img);
 
-      //std::cout << lines.size() << " mid lines" << std::endl;
-      //std::cout << linesred.size() << " side lines" << std::endl;
       // start and end of the lines stored as points
       for (auto i : left_lines)
       {
@@ -429,7 +319,6 @@ void mZEDDetection::Update()
         left_b = cv::Point(left_line[2], left_line[3]);
       }
       // start and end of the lines stored as points
-      ////std::cout<<mid_lines.size()<<std::endl;
       for (auto i : mid_lines)
       {
         ini = cv::Point(i[0], i[1]);
@@ -470,34 +359,13 @@ void mZEDDetection::Update()
       // calculate start and finish points for right, mid and left lines
       double right_ini_x = ((ini_y - right_b.y) / right_m) + right_b.x;
       double right_fini_x = ((fini_y - right_b.y) / right_m) + right_b.x;
-      //std::cout << right_ini_x << " ";
-      //std::cout << right_fini_x << " ";
-      //std::cout << right_m << std::endl;
 
       double mid_ini_x = ((ini_y - mid_b.y) / mid_m) + mid_b.x;
       double mid_fini_x = ((fini_y - mid_b.y) / mid_m) + mid_b.x;
 
-      //std::cout << mid_ini_x << " " ;
-      //std::cout << mid_fini_x << " " ;
-      //std::cout << mid_m << std::endl;
-
       double left_ini_x = ((ini_y - left_b.y) / left_m) + left_b.x;
       double left_fini_x = ((fini_y - left_b.y) / left_m) + left_b.x;
 
-      //*/
-
-      /*
-      double ini_y = 376;
-      double fini_y = 240;
-      std::vector<double> mid = MidDetection();
-      std::vector<double> sides = SideDetection();
-      double left_ini_x = sides[0];
-      double left_fini_x = sides[1];
-      double right_ini_x = sides[2];
-      double right_fini_x = sides[3];
-      double mid_ini_x = mid[0];
-      double mid_fini_x = mid[1];
-      */
 
       //  draw the lines
       cv::line(img, cv::Point(left_ini_x, ini_y), cv::Point(left_fini_x, fini_y), cv::Scalar(0, 0, 255, 255), 5, CV_AA);
@@ -511,50 +379,11 @@ void mZEDDetection::Update()
       old_mid = mid_ini_x - midpixel;
       old_right = right_ini_x - midpixel;
 
-      if (queueLeft.size() < 999 && old_left != std::numeric_limits<double>::infinity())
-      {
-        queueLeft.push(old_left);
-      }
-      else if (old_left != std::numeric_limits<double>::infinity())
-      {
-        queueLeft.push(old_left);
-        this->angle_to_left_out.Publish(std::acos((queueLeft.back() - queueLeft.front()) / (std::sqrt(std::pow(queueLeft.back() - queueLeft.front(), 2) + 100))));
-        queueLeft.pop();
-      }
-
-      if (queueMid.size() < 999 && old_mid != std::numeric_limits<double>::infinity())
-      {
-        queueMid.push(old_mid);
-      }
-      else if (old_mid != std::numeric_limits<double>::infinity())
-      {
-        queueMid.push(old_mid);
-        this->angle_to_mid_out.Publish(std::acos((queueMid.back() - queueMid.front()) / (std::sqrt(std::pow(queueMid.back() - queueMid.front(), 2) + 100))));
-        queueMid.pop();
-      }
-      if (queueRight.size() < 999 && old_right != std::numeric_limits<double>::infinity())
-      {
-        queueRight.push(old_right);
-      }
-      else if (old_right != std::numeric_limits<double>::infinity())
-      {
-        queueRight.push(old_right);
-        this->angle_to_right_out.Publish(std::acos((queueRight.back() - queueRight.front()) / (std::sqrt(std::pow(queueRight.back() - queueRight.front(), 2) + 100))));
-        queueRight.pop();
-      }
 
       this->distance_to_left_out.Publish(old_left);
       this->distance_to_mid_out.Publish(old_mid);
       this->distance_to_right_out.Publish(old_right);
 
-
-      //M = old_mid;
-
-      // Make one image
-      //usleep(100000);
-
-
-      //this->camera_out.Publish(img_out);
 
     }
   }
